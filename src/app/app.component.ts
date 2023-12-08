f7566f61a9d6136df542f65fce647b38855145ce
NIRMAL GHOSH <145506351+techno-ghosh@users.noreply.github.com>
2023-12-08 14:21:14 -0500

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   title = 'accessible-website';
- 
+  route: string;
+  currentURL='';
 
+  constructor(private router: Router) { 
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+  )
+      .subscribe((event: any) => {
+        
+        console.log(event.url);
+        
 
-  constructor() {
+        switch(event.url) {
+          case '/' : this.title = 'Home Page';
+          break;
+          case '/services': this.title = 'Services';
+          break;
+          case '/schedule-call': this.title = 'Schedule Call';
+          break;
+          default : this.title = 'Accessible Project';
+          break;
+        }
 
+      });
+  
+      
   }
+
+
+
 
  
 
